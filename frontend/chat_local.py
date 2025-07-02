e63ac1ca9424d9ea6440c483a9e76b0950bff267
Zeeshanunique <zeeshanunique2619@email.com>
2025-07-02 20:51:28 +0000

    Jul 3, 2025, 2:21 AM

--- frontend/chat_local.py
+++ frontend/chat_local.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+
 from ralph.graph import build_graph, AgentState
 from langchain_core.messages import AIMessageChunk, HumanMessage
 from typing import AsyncGenerator, Any
